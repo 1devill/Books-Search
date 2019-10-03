db0b2e9ae9f88c1e178a7bfddda75c6e737a4d08
Daniil <dannydeceiver@gmail.com>
2019-10-03 22:19:58 +0300

    2nd commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 "use strict";
 
 let $searchForm = $("#search-form");
+let $bookList = $('#book-list');
+let $currentBook = $('#current-book');
+let $bookDescription = $(".book-description");
+let books = [];
 
 $searchForm.on("submit", function(event) {
     event.preventDefault();
@@ -19,8 +23,46 @@ function getBooks(query) {
         method: "GET",
         data: `q=${query}`
     }).done((response) => {
-        console.log(response);
+        books = response.items;
+        addBooks(response.items);
     }).fail((error) => {
         console.log(error);
     })
 }
+
+
+function addBooks(data) {
+    $bookList.empty();
+
+    data.forEach(book => {
+        $("<a href=''>").addClass("list-group-item")
+            .text(book.volumeInfo.title)
+            .attr("data-id", book.id)
+            .appendTo($bookList);
+    });
+}
+
+$bookList.on("click", "[data-id]", function(event) {
+    event.preventDefault();
+
+    let bookId = $(this).data("id");
+
+    let book = books.find((item) => item.id === bookId);
+    $currentBook.fadeIn();
+
+    $currentBook.find(".book-title").text(`${book.volumeInfo.title} 
+    | ${book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "No author"}
+    ${book.volumeInfo.publishedDate}`)
+
+    $bookDescription.empty();
+    $("<img>").attr("src", book.volumeInfo.imageLinks.thumbnail)
+        .appendTo($bookDescription);
+
+    $("<p>").text(book.volumeInfo.description).appendTo($bookDescription);
+
+    $("<a>").attr("href", book.volumeInfo.previewLink)
+        .attr("target", "_blank")
+        .text("Read more...")
+        .addClass("read-link")
+        .appendTo($bookDescription);
+});
